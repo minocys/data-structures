a0678884632b54ead23b9ab5aa6e28fa7e65dd99
minocys <harich.leung@gmail.com>
2014-12-11 14:49:53 -0500

    finished prototypal

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,46 @@
 var Queue = function(){
+  var newQueue = {};
+  newQueue.length = 0;
+  newQueue.first = 0;
+  newQueue.key = 0;
+  newQueue.storage = {};
+
+  _.extend(newQueue, queueMethods);
+
+  return newQueue;
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 };
 
 var queueMethods = {};
 
+queueMethods.size = function(){
+  return this.length;
 
+};
+
+queueMethods.enqueue = function(value){
+  this.length++;
+  this.key++;
+  this.storage[this.key] = value;
+  if(this.length === 1){  
+    this.first = this.key;
+  }
+};
+
+queueMethods.dequeue = function() {
+  if(this.length === 0){
+   return undefined;
+  };
+
+  var item = this.storage[this.first];
+  delete this.storage[this.first];
+  this.length--;
+  if(this.length > 0){  
+    this.first++;
+  }
+
+  return item;
+  
+};
 
--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,47 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newQueue = Object.create(queueMethods);
+  newQueue.length = 0;
+  newQueue.head = 0;
+  newQueue.key = 0;
+  newQueue.storage = {};
+  return newQueue;
 };
 
 var queueMethods = {};
 
+queueMethods.size = function(){
+  return this.length;
+}
 
+queueMethods.enqueue = function(value){
+  //increment length
+  this.length++;
+  //increment key
+  this.key++;
+  //storage[key] = value
+  this.storage[this.key] = value;
+  if(this.length === 1){
+    //set head to first value
+    this.head = this.key;
+  }
+}
+
+
+queueMethods.dequeue = function(){
+  //if length === 0
+  if(this.length === 0){
+    return undefined;
+  }
+  var item = this.storage[this.head];
+  delete this.storage[this.head];
+  //decrement length
+  this.length--;
+  //if length > 0
+  if(this.length > 0){
+  //increment head
+    this.head++;
+  }
+  return item;
+}
