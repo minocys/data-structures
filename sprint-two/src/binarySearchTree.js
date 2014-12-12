2d7901b6fffee9d5d3dd570ce0f2581b863ecdf3
minocys <harich.leung@gmail.com>
2014-12-12 12:44:23 -0500

    completed binary!

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -8,32 +8,48 @@ var BinarySearchTree = function(value){
 
 var binaryMethods = {
   insert : function(value){
-    //recursive logic
-    console.log(this);
-    if (value >= this.value){
-      if (this.right !== null){ 
-        this.right.insert(value);
+    //recursive
+    if (value > this.value) {
+      if(this.right === null){
+        this.right = BinarySearchTree(value);
       } else {
-        var seed = BinarySearchTree(value);
-        this.right = seed;
+        this.right.insert(value);
       }
-    } else if (value < this.value) {
-      if(this.left !== null){
+    }
+    if (value < this.value) {
+      if(this.left === null){
+        this.left = BinarySearchTree(value);
+      } else {
         this.left.insert(value);
-      } else
-        var seed = BinarySearchTree(value);
-        this.left = seed;
+      }
     }
   },
 
+  //although the tree may be different due to insertion sequence
+  //placement of numbers will be the same
   contains : function(target){
-
+    if(target === this.value){
+      return true;
+    }
+    //if target is > this.value
+    if(target > this.value && this.right){
+      return this.right.contains(target);
+    }
+    if(target < this.value && this.left){
+      returnthis.left.contains(target);
+    }
+    return false;
   },
 
-  depthFirstLog : function(){
-
-  }
-
+  depthFirstLog : function(func){
+      func(this.value);
+      if(this.left){
+        this.left.depthFirstLog(func);
+      }
+      if(this.right){
+        this.right.depthFirstLog(func);
+      }
+  },
 };
 
 
