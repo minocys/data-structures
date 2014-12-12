ae412e6d073e7ee3ed0cdc210e8ca813fa0449d3
minocys <harich.leung@gmail.com>
2014-12-11 23:19:14 -0500

    finished doubly linked

--- sprint-two/spec/linkedListSpec.js
+++ sprint-two/spec/linkedListSpec.js
@@ -51,5 +51,22 @@ describe('linkedList', function() {
     expect(linkedList.contains(4)).to.equal(false);
   });
 
+  it('should designate a new head when new nodes are added', function(){
+    linkedList.addToHead(4);
+    expect(linkedList.head.value).to.equal(4);
+    linkedList.addToHead(5);
+    expect(linkedList.head.value).to.equal(5);
+  });
+
+  it('should remove the tail from the list when removeTail is called', function(){
+    linkedList.addToTail(4);
+    linkedList.addToTail(5);
+    expect(linkedList.tail.value).to.equal(5);
+    linkedList.removeTail();
+    expect(linkedList.tail.value).to.equal(4);
+  });
+
+
+
   // add more tests here to test the functionality of linkedList
 });
--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,25 +4,81 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var newNode = Node(value);
+
+    if (this.tail === null){
+      this.tail = newNode;
+      // the first time add to tail is called, head needs to be set
+      this.head = newNode;
+    } else {
+      //set previous item next newNode
+      this.tail.next = newNode;
+      newNode.previous = this.tail;
+      //set tail to newNode
+      this.tail = newNode;
+    }
   };
 
   list.removeHead = function(){
+    // store "item value" in a new variable
+    var previousHead = this.head.value;
+    // head needs to be set
+    this.head = this.head.next;
+
+    return previousHead;
   };
 
   list.contains = function(target){
+    var node = this.head;
+    //iterate
+    // a loop that goes through node.next
+    while ( node.value !== target) {
+      if ( node === this.tail ) {
+        return false;
+      }
+      node = node.next;
+    }
+    return true;
+  };
+
+  list.addToHead = function(value){
+    // takes a value and adds it to the front of the list.
+    var newNode = Node(value);
+
+    if (this.head === null){
+      this.head = newNode;
+      // the first time add to tail is called, head needs to be set
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;  // maybe??
+      //set previous item next newNode
+      this.head.previous = newNode;
+      //set tail to newNode
+      this.head = newNode;
+    }
+  };
+
+  list.removeTail = function(){
+    var previousTail = this.tail.value;
+    this.tail = this.tail.previous;
+    return previousTail;
   };
 
   return list;
 };
 
 var Node = function(value){
+  // give node a reference to previous node
+  // node.previous = null;
   var node = {};
 
   node.value = value;
   node.next = null;
+  node.previous = null;
 
   return node;
 };
+
 
 /*
  * Complexity: What is the time complexity of the above functions?
